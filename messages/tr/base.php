29e55250b9032b50563e17403d18486f6ef617b0
Çağatay <cagatayldzz@gmail.com>
2018-08-03 09:29:39 +0300

    Create base.php

---
+++ messages/tr/base.php
@@ -0,0 +1,8 @@
+<?php
+  return [
+    '<strong>Google+</strong>' => '<strong>Google+</strong>',
+    'Google+ Settings' => 'Google+ Ayarlar',
+    'Google+ profile URL:' => 'Google+ profil URL:',
+    '<strong>Google+</strong> module configuration' => '<strong>Google+</strong> modül yapılandırması',
+    'Save' => 'Kaydet',
+  ];
